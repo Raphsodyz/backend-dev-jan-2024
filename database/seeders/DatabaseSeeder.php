e7f3e6c89dd80ab4fc712eb6c43e9e94e743aabd
Arthur Vinícius <arthurvsr@gmail.com>
2024-01-06 23:27:02 -0300

    Feat(seeder): database seeder from json

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,13 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Http;
+use App\Models\Municipio;
+use App\Models\Estado;
+use Illuminate\Support\Facades\DB;
+use Exception;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +20,43 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        try
+        {
+            $idMG = Estado::select('id')->where('nome_estado', '=', 'MG')->value('id');
+            $response_mg = Http::get('https://raw.githubusercontent.com/tbrugz/geodata-br/master/geojson/geojs-31-mun.json');
+            self::populateDatabase($response_mg, $idMG);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+            $idSP = Estado::select('id')->where('nome_estado', '=', 'SP')->value('id');
+            $response_sp = Http::get('https://raw.githubusercontent.com/tbrugz/geodata-br/master/geojson/geojs-35-mun.json');
+            self::populateDatabase($response_sp, $idSP);
+        }
+        catch(Exception $ex)
+        {
+            echo "An exception was throw: " . $ex->getMessage();
+        }
+    }
+
+    private static function populateDatabase($requisicion, $idState)
+    {
+        DB::transaction(function() use ($requisicion, $idState){
+            if(!$requisicion->successful())
+            throw new Exception("The json file for seed the database is unavailable.");
+        
+            $data = $requisicion->json();
+            if(!isset($data['features']) && !is_array($data['features']))
+                throw new Exception("The json schema has changed.");
+                
+            foreach($data['features'] as $value){
+                $geometryData = json_encode($value['geometry']);
+                $municipio = [
+                    'id' => Str::uuid()->toString(),
+                    'nome_municipio' => $value['properties']['name'],
+                    'geom' => DB::raw("public.ST_SetSRID(public.ST_GeomFromGeoJSON('$geometryData'), 4326)"),
+                    'id_state' => $idState
+                ];
+                $model = new Municipio($municipio);
+                $model->save();
+            }
+        });
     }
 }
