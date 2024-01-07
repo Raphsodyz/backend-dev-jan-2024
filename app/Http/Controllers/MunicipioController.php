af67b2fa0fa63a43a5d6c7852abc761d5b43336d
Arthur Vinícius <arthurvsr@gmail.com>
2024-01-07 11:53:09 -0300

    Feat: Municipio controller and api routes

---
+++ app/Http/Controllers/MunicipioController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Municipio;
+use Exception;
+use Illuminate\Support\Facades\DB;
+
+/**
+ * @OA\Info(
+ *      title="TerraQ test endpoints",
+ *      version="1.0.0",
+ *      description="API for interation with data.",
+ * )
+ */
+class MunicipioController extends Controller
+{
+    /** 
+     * @OA\Get(
+     *      path="/api/localizar-municipio",
+     *      summary="Method for find municipio by his lat/lon.",
+     *      tags={"Municipios Controller"},
+     *      @OA\Parameter(
+     *          in="query",
+     *          name="longitude",
+     *          @OA\Schema(type="double"),
+     *      ),
+     *      @OA\Parameter(
+     *          in="query",
+     *          name="latitude",
+     *          @OA\Schema(type="double"),
+     *      ),
+     *      @OA\Response(response="200", description="Data found!"),
+     *      @OA\Response(response="404", description="Resource not found with the informed lat/lon."),
+     *      @OA\Response(response="500", description="Unavailable service.")
+     * )
+    */
+    public function GetMunicipio(Request $request)
+    {
+        try
+        {
+            $latitude = $request->input('latitude');
+            $longitude = $request->input('longitude');
+
+            $point = DB::raw("ST_MakePoint($longitude, $latitude)");
+            $result = Municipio::whereRaw("ST_Contains(geom, $point)")->first();
+
+            if(!$result)
+                return response()->json(['Error' => 'Not found municipio with these lat/lon.'], 404);
+                
+            return response()->json([$result], 200);
+        }
+        catch(Exception $ex)
+        {
+            return response()->json(['Error' => $ex->getMessage()], 500);
+        }
+    }
+}
