176c6a1ad59382accf14777b2e36731e1964a792
Arthur Vinícius <arthurvsr@gmail.com>
2024-01-07 12:19:36 -0300

    Fix(api.php): api sufix name

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/api/localizar-municipio', [MunicipioController::class, 'GetMunicipio']);
+Route::get('/localizar-municipio', [MunicipioController::class, 'GetMunicipio']);
